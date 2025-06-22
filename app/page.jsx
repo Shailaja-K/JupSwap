3b8ecb4594d8fafc0905790f4f97eae67292f18e
Shailaja-K <132611255+Shailaja-K@users.noreply.github.com>
2025-06-22 23:22:05 +0530

    Delete app/page.jsx

--- app/page.jsx
+++
@@ -1,7 +0,0 @@
-export default function Page() {
-  return (
-    <div className="flex items-center justify-center min-h-screen text-xl font-bold">
-      Welcome to JupSwap - Solana Swap Interface
-    </div>
-  );
-}
