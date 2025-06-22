4ac6730e4a6c377b496f362f767f0f8ba217a309
Shailaja-K <132611255+Shailaja-K@users.noreply.github.com>
2025-06-22 23:01:01 +0530

    Add files via upload

---
+++ pages/index.js
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="flex items-center justify-center min-h-screen text-xl font-bold">
+      Welcome to JupSwap - Solana Swap Interface
+    </div>
+  );
+}
